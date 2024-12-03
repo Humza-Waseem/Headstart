046c1126370a1fb0859d1e9a1dd59df5bd9dedad
Malik <mudassirnaveed303@gmail.com>
2024-12-03 11:13:43 +0500

    Dark/Light

--- static/js/script.js
+++ static/js/script.js
@@ -234,3 +234,19 @@ document.querySelectorAll('.colorPallete button').forEach((button, index) => {
   });
 });
 
+
+
+// Light and dark mode
+const themeToggle = document.getElementById('theme-toggle');
+// Load saved theme from localStorage
+const currentTheme = localStorage.getItem('theme') || 'light';
+document.documentElement.setAttribute('data-theme', currentTheme);
+themeToggle.textContent = currentTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+
+// Toggle theme on button click
+themeToggle.addEventListener('click', () => {
+    const newTheme = document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+    themeToggle.textContent = newTheme === 'dark' ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+});
